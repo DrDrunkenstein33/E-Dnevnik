44d1fea2ef0c88c8e4c2e5ab154f11aa9c3c5803
DrDrunkenstein33 <93375449+DrDrunkenstein33@users.noreply.github.com>
2022-04-29 00:22:37 +0200

    Add files via upload

---
+++ sifar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Ednevnik
+{
+    public partial class Sifarnik : Form
+    {
+        DataTable tabela;
+        SqlDataAdapter adapter;
+        string ime_tabele;
+        public Sifarnik(ime_tabele)
+        {
+            ime_tabele = tabela;
+            InitializeComponent();
+        }
+        private void Sifarnik_Load(object Sender, EventArgs e)
+        {
+            adapter = new SqlDataAdapter("SELECT * FROM " + ime_tabele, Konekcija.connect());
+            tabela = new DataTable();
+            adapter.Fill(tabela);
+            dataGridView1.DataSource = tabela;
+            dataGridView1.Columns["id"].ReadOnly = true;
+        }
+        private void btn_ok_Click(object Sender, EventArgs e)
+        {
+            DataTable menjaj = tabela.GetChanges();
+            adapter.UpdateCommand = new SqlCommandBuilder(adapter).GetUpdateCommand();
+            if(menjaj != null)
+            {
+                adapter.Update(menjaj);
+                this.Close();
+            }
+        }
+    }
+}
